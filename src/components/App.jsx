f8146583116fae72c6dc800b6f3dc8cc28823533
vovaukrainets <vovaukrainets@gmail.com>
2023-02-13 00:02:42 +0200

    cool
    fhfh

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import Profile from 'src/Profile/Profile';
+// import Statistics from 'components/Statistics/Statistics';
+// import FriendList from 'components/FriendList/FriendList';
+// import TransactionHistory from 'components/TransactionHistory/TransactionHistory';
+
+import cardInfo from 'srs/Profile/user.json';
+// import data from 'components/Statistics/data.json';
+// import friends from 'components/FriendList/friends.json';
+// import transactions from 'components/TransactionHistory/transactions.json';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        avatar={cardInfo.avatar}
+        username={cardInfo.username}
+        tag={cardInfo.tag}
+        location={cardInfo.location}
+        followers={cardInfo.stats.followers}
+        views={cardInfo.stats.views}
+        likes={cardInfo.stats.likes}
+      />
+      {/* <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} /> */}
     </div>
   );
 };
